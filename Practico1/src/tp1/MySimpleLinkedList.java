514ce3c6777f9bd5de56b4aa624607d97bc535aa
Agustin Cidoni <agustincidoni@gmail.com>
2026-03-16 19:33:34 -0300

    Estructura base de Lista Enlazada

---
+++ Practico1/src/tp1/MySimpleLinkedList.java
@@ -0,0 +1,63 @@
+package tp1;
+
+public class MySimpleLinkedList<T> {
+	
+	private Node<T> first;
+	private int size;
+	
+	public MySimpleLinkedList() {
+		this.first = null;
+		this.size  = 0;
+	}
+	
+	public void insertFront(T info) {
+		Node<T> tmp = new Node<T>(info,null);
+		tmp.setNext(this.first);
+		this.first = tmp;
+		this.size++;
+	}
+	
+	public T extractFront() {		
+		if (this.first == null) return null;
+		T info = this.first.getInfo();
+		this.first = this.first.getNext();
+		this.size--;
+		return info;
+	}
+
+	public boolean isEmpty() {
+		return this.first == null;
+	}
+	
+	public T get(int index) {
+		if(index < 0 || index > this.size) return null;
+		Node<T> node = this.first;
+		int idx = 0;
+		while(idx < index) {
+			node = node.getNext();
+			idx++;
+		}
+		return node.getInfo();
+		
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	
+	@Override
+	public String toString() {
+	    String s = "[";
+	    Node<T> node = this.first;
+
+	    while (node != null) {
+	        s += node.getInfo();
+	        node = node.getNext();
+	        if (node != null) s += ", ";
+	    }
+
+	    s += "]";
+	    return s;
+	}
+	
+}
